83c876b90b0464a7c3be3748b815cf84dfc91fbd
Shyaar Sati Fwatshak <104396521+Shyaar@users.noreply.github.com>
2024-12-08 18:40:24 +0100

    Revert "Sendii"

--- build/script.js
+++ build/script.js
@@ -111,20 +111,3 @@
     );
     });
 }
-
-
-//Sendi call button function
-
-document.addEventListener(`DOMContenLoaded`, function() {
-    const callButtons = document.querySelectorAll(`.call-btn`);
-
-    callButtons.forEach(button => {
-        button.addEventListener(`click`, function() {
-            const phoneNumber = button.getAttribute(`data-phone`)
-
-            window.location.href = `tel:${phoneNumber}`;
-        });
-    });
-}) 
-
-
